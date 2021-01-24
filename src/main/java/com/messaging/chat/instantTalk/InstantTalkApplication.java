f0fb1a56a533319a47809c84526170b823f86233
albinvinoy <albinvinoy@gmail.com>
2021-01-23 20:02:43 -0800

    init push

---
+++ src/main/java/com/messaging/chat/instantTalk/InstantTalkApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.chat.instantTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messaging/chat/instantTalk/InstantTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.chat.instantTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
